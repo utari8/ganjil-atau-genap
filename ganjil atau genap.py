5788274337783f7ed26c1569c16ee5df10179733
F. N. Utari <77846876+utari8@users.noreply.github.com>
2021-12-13 19:30:02 +0700

    Add files via upload

---
+++ ganjil atau genap.py
@@ -0,0 +1,10 @@
+# code by utari8
+
+no = int(input("Masukkan angka : "))
+nomer = no%2
+if nomer == 0:
+    print("Angka ",no," adalah genap")
+elif nomer == 1:
+    print("Angka ",no," adalah ganjil")
+
+# code by utari8
